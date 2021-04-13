d2a1f5cc0b32bc76d3b04ce3b7d2c8dec2d2b226
Vana-Vanch <vanavanch98@gmail.com>
2021-04-13 13:14:17 +0530

    added 11 and 12

---
+++ 12/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class jagged_arrays
+    {
+        public void jagged_arr()
+        {
+            int[][] j_arr = {
+                new int[] {1,2,3},
+                new int[] {4,5,6,7,8},
+                new int[] {9,10,11}
+            };
+            for (int i = 0; i < j_arr.Length; i++)
+            {
+                for (int j = 0; j < j_arr[i].Length; j++)
+                {
+                    Console.Write(j_arr[i][j] + ",");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class two_dimentional_arr
+    {
+        public void two_dim()
+        {
+            int[,] two_arr = new int[3, 4] {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12}
+            };
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(two_arr[i, j] + ",");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            jagged_arrays obj = new jagged_arrays();
+            Console.WriteLine("Jagged arrays:");
+            obj.jagged_arr();
+            Console.WriteLine("Two dimentional arrays");
+            two_dimentional_arr obj2 = new two_dimentional_arr();
+            obj2.two_dim();
+
+        }
+    }
+}
+
---
+++ prog11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace prog11
+{
+    class Parent
+    {
+        public void show()
+        {
+            Console.WriteLine("This is a parent class");
+        }
+    }
+    class new_child : Parent
+    {
+        new public void show()
+        {
+            Console.WriteLine("Can i override");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new_child obj2 = new new_child();
+            obj2.show();
+            Parent obj1 = new Parent();
+            obj1.show();
+
+            Console.ReadLine();
+        }
+    }
+}
+
