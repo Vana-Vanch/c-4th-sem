64ac95b8bf893fc914b60f5e33b28f16a8dbe62b
Vana-Vanch <vanavanch98@gmail.com>
2021-04-13 20:48:28 +0530

    sawmhnihnaaa

---
+++ 20/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Test
+    {
+        public delegate int NewDelegate(int x, int y);
+
+        static void Display(NewDelegate d)
+        {
+            Console.WriteLine(d(10, 20));
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            NewDelegate d1 = new NewDelegate(Add);
+            Display(d1);
+            Console.ReadKey();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
