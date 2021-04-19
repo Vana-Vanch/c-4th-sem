ad875e4a985248590e76b9f24fb4c79a1e7db74d
Vana-Vanch <vanavanch98@gmail.com>
2021-04-19 22:45:43 +0530

    added program 2 and 3

---
+++ windows form/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int vowel = 0;
+            int consonant = 0;
+            int special_symbol = 0;
+            int number = 0;
+            string textbox = textBox1.Text;
+            for (int i = 0; i < textbox.Length; i++)
+            {
+                if (textbox[i] == 'a' || textbox[i] == 'e' || textbox[i] == 'i' || textbox[i] == 'o' || textbox[i] == 'u')
+                {
+                    vowel++;
+                }
+                else if (textbox[i] >= 'a' && textbox[i] <= 'z')
+                {
+                    consonant++;
+                }
+                else if (textbox[i] >= '0' && textbox[i] <= '9')
+                {
+                    number++;
+                }
+                else
+                {
+                    special_symbol++;
+                }
+
+            }
+            label1.Text += vowel;
+            label2.Text += consonant;
+            label3.Text += number;
+            label4.Text += special_symbol;
+        }
+    }
+}
